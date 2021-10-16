1639c690732baf33b79f9aa2e24f0da128f4a091
Steve Astels <sastels@gmail.com>
2021-10-16 13:29:14 -0400

    Initial Commit

---
+++ FindWordTimes/FindWordTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindWordTimesApp.swift
+//  FindWordTimes
+//
+//  Created by Stephen Astels on 2021-10-16.
+//
+
+import SwiftUI
+
+@main
+struct FindWordTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
